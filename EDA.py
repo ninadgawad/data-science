48773d0803e546c0a52995f34fde8f8084d3535d
Ninad Gawad <ninadgawad@gmail.com>
2022-01-08 17:53:27 -0500

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -1,5 +1,16 @@
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
+# Load City information to data frame
+df = pd.read_csv("City.csv")
 
+# Apply basic statistical computations using pandas describe() function 
+dF.describe()
 
+# Get Counts for a specific column
+dF["city_name"].value_counts()
+
+y = list(df.peopleCount)
+plt.boxplot(y)
+plt.show()
