d5f668bb37f0715f68eacb20b9ebe453915cf62a
Ninad Gawad <ninadgawad@gmail.com>
2022-01-23 12:13:53 -0500

    Update 02_PythonPandas.py

--- 02_PythonPandas.py
+++ 02_PythonPandas.py
@@ -2,14 +2,15 @@ import os
 import pandas as pd
 import json
 
-def convertdict2dataframe(dict):
-  return pd.DataFrame.from_dict(dict)  
+
+def dict_to_dataframe(dict):
+    return pd.DataFrame.from_dict(dict, orient='index')
 
 
 if __name__ == '__main__':
-  print("-" * 45)
-  print("\t Main Function")
-  print("-" * 45)
-  dict = {"a":"10","b":"20","c":"30"}
-  df = convertdict2dataframe(dict)
-  print(df.head(5))
+    print("-" * 44)
+    print("\t Main Function")
+    print("-" * 44)
+    data = {'a':'10', 'b':'20', 'c':'30'}
+    df = dict_to_dataframe(data)
+    print(df.head(5))
